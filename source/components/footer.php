71c51120af32886bfbe7992a9ad9c68ed4cd58fe
Pavlo Maiorov <pmayorov@gmail.com>
2023-03-20 19:55:24 +0200

    Адаптивная сетка формы - 60%

---
+++ source/components/footer.php
@@ -0,0 +1,37 @@
+<footer class="page__footer footer">
+    <!-- Хештег Седоны-->
+    <a class="footer__hashtag title" href="#">#Visitsedona</a>
+    <!-- Меню соцсетей -->
+    <ul class="footer__list socials">
+      <li class="footer__item">
+        <a class="socials__link social__link--twitter" href="#">
+          <span class="visually-hidden">Twitter</span>
+          <svg width="18" height="15" aria-hidden="true">
+            <use xlink:href="./img/icons/sprite.svg#twitter"></use>
+          </svg>
+        </a>
+      </li>
+      <li class="footer__item">
+        <a class="socials__link social__link--facebook" href="#">
+          <span class="visually-hidden">Facebook</span>
+          <svg width="9" height="19" aria-hidden="true">
+            <use xlink:href="./img/icons/sprite.svg#facebook"></use>
+          </svg>
+        </a>
+      </li>
+      <li class="footer__item">
+        <a class="socials__link social__link--youtube" href="#">
+          <span class="visually-hidden">YouTube</span>
+          <svg width="20" height="14" aria-hidden="true">
+            <use xlink:href="./img/icons/sprite.svg#youtube"></use>
+          </svg>
+        </a>
+      </li>
+    </ul>
+    <!-- Логотип академии -->
+    <a class="footer__copyright html-academy" href="#">Разработано
+      <svg class="html-academy__logo" width="27" height="34" role="img" aria-hidden="true">
+        <use xlink:href="./img/icons/sprite.svg#html-academy"></use>
+      </svg>
+    </a>
+  </footer>
